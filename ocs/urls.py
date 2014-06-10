bf197b3233c98cfd2a2a607721a4cbdd0574783d
hypertexthero <simon@hypertexthero.com>
2014-06-10 10:04:13 +0200

    more ocs work

--- ocs/urls.py
+++ ocs/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from mezzanine.core.views import direct_to_template
+from django.contrib.auth.decorators import login_required
 
 # from ocs import views
 
 urlpatterns = patterns('',
     url(r'^$', direct_to_template, {"template": "ocs/home.html"}, name="home"),
-    url(r'^upload/$', direct_to_template, {"template": "upload.html"}, name="upload"),
-    url(r'^detail/$', direct_to_template, {"template": "detail.html"}, name="detail"),
+    url(r'^upload/$', direct_to_template, {"template": "ocs/upload.html"}, name="upload"),
+    url(r'^001-test-document-detail/$', login_required(direct_to_template), {"template": "ocs/detail.html"}, name="detail"),
     # url(r'^$', views.IndexView.as_view(), name='index'),
     # url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
     # url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
