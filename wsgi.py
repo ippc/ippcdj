2687a7c50a2ce28760351b42ac026af85e3e5baa
Simon Griffee <simon@hypertexthero.com>
2015-02-18 10:04:17 -0500

    wsgi update to try to fix gunicorn error

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
 
 import os
+import sys
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+sys.path.append('/work/projects/ippcdj-env/ippcdj_repo')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
