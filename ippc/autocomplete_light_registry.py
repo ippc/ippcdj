0bc93f5da951bae21ecbce37da5fdad859c27a81
Paola Sentinelli <paola.sentinelli@gmail.com>
2016-03-23 12:05:49 +0100

    no changes

--- ippc/autocomplete_light_registry.py
+++ ippc/autocomplete_light_registry.py
@@ -7,7 +7,7 @@ from t_eppo.models import Names
 autocomplete_light.register(EppoCode, search_fields=('codename',),
                autocomplete_js_attributes={'placeholder': '....'})
 autocomplete_light.register(Names, search_fields=('codename',),
-               autocomplete_js_attributes={'placeholder': '....'})
+               autocomplete_js_attributes={'placeholder': '....',})
 autocomplete_light.register(IssueKeyword, search_fields=('name',),
                autocomplete_js_attributes={'placeholder': '....'})
 autocomplete_light.register(CommodityKeyword, search_fields=('name',),
